47c5d5c0b2b9c725a97d49fe5398de450fdf3cb9
daniel <mio0115@github.com>
2024-10-13 21:07:24 +0800

    add save_as parameter

--- src/utils/arg_parser.py
+++ src/utils/arg_parser.py
@@ -57,6 +57,9 @@ def get_parser():
         dest="path_to_valid_data",
         help="path to the validation dataset",
     )
+    parser.add_argument(
+        "--save_as", type=str, default="model_state.pth", dest="save_as"
+    )
     parser.add_argument("--augment_factor", type=int, default=5, dest="augment_factor")
     parser.add_argument(
         "--train", action="store_true", dest="train", help="train the model"
