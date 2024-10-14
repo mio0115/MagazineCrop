082248e7b823cb2a73d8e55657e37990546ab483
daniel <mio0115@github.com>
2024-10-14 14:55:41 +0800

    modify; to underscore

--- src/utils/arg_parser.py
+++ src/utils/arg_parser.py
@@ -15,7 +15,7 @@ def get_parser():
     )
     parser.add_argument(
         "-bs",
-        "--batch-size",
+        "--batch_size",
         type=int,
         default=12,
         dest="batch_size",
@@ -23,7 +23,7 @@ def get_parser():
     )
     parser.add_argument(
         "-lr",
-        "--learning-rate",
+        "--learning_rate",
         type=float,
         default=1e-3,
         dest="learning_rate",
