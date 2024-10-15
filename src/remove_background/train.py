917a3cfdcf42a75b8751d823e73d11c5ad27fd02
daniel <mio0115@github.com>
2024-10-15 16:11:12 +0800

    modify; try another lr_scheduler

--- src/remove_background/train.py
+++ src/remove_background/train.py
@@ -21,7 +21,8 @@ def train(
     valid: bool = True,
 ):
     print("Training model...")
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.95)
+    # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.95)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 15, 20])
     model = model.to(args.device)
     path_to_save = os.path.join(os.getcwd(), "checkpoints", args.save_as)
 
