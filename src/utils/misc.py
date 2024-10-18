98ae822a7e6c5a8b99b17c5dc1ecb6598fd24a49
daniel <mio0115@github.com>
2024-10-18 20:51:14 +0800

    add polygon_to_mask

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+import cv2
 
 
 def to_onehot(tensor: torch.Tensor) -> torch.Tensor:
@@ -6,3 +8,11 @@ def to_onehot(tensor: torch.Tensor) -> torch.Tensor:
     onehot.scatter(1, tensor.unsqueeze(1), 1)
 
     return onehot
+
+
+def polygon_to_mask(polygon, height: int, width: int):
+    mask = np.zeros((height, width), dtype=np.uint8)
+    pts = np.array(polygon, np.int32)
+    bin_mask = cv2.fillConvexPoly(mask, pts, 1).astype(np.int64)
+
+    return bin_mask
