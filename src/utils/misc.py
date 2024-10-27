3916a485ac990221b4d2839958a52a559d35419c
daniel <mio0115@github.com>
2024-10-27 19:08:04 +0800

    add lines_to_coord

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -16,3 +16,29 @@ def polygon_to_mask(polygon, height: int, width: int):
     bin_mask = cv2.fillConvexPoly(mask, pts, 1).astype(np.int64)
 
     return bin_mask
+
+
+def lines_to_coord(lines, height: int, width: int):
+    intersections = []
+    for line in lines:
+        (x1, y1), (x2, y2) = line["points"]
+        if x1 == x2:
+            intersections.append((x1, np.pi / 2))
+            continue
+        if y1 > y2:
+            y1, y2 = y2, y1
+            x1, x2 = x2, x1
+
+        slope, bias = (y1 - y2) / (x1 - x2), y1 - (y1 - y2) / (x1 - x2) * x1
+
+        if slope > 0:
+            intersections.append((1 / slope * (height / 2 - bias), np.arctan(slope)))
+        else:
+            intersections.append(
+                (
+                    1 / slope * (height / 2 - bias),
+                    np.pi - np.arctan(np.abs(slope)),
+                )
+            )
+
+    return intersections
