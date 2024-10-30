0cd900b991c6e33b5be6f6b4257640720c77c700
daniel <mio0115@github.com>
2024-10-30 14:44:11 +0800

    modify; add unet_pp to model in split_page

--- src/split_page/model/model.py
+++ src/split_page/model/model.py
@@ -2,21 +2,26 @@ import torch
 from torch import nn
 import torchvision as tv
 
+from ...model_templates import templates
+
 
 class SimpleLineEstimator(nn.Module):
     def __init__(self, backbone: nn.Module, *args, **kwargs):
         super(SimpleLineEstimator, self).__init__(*args, **kwargs)
 
         self._backbone = backbone
+        self._avg_pool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
         self._x_reg_head = nn.Sequential(
+            nn.Linear(32, 1024),
+            nn.LeakyReLU(),
             nn.Linear(1024, 512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(512, 256),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(256, 1),
         )
         self._theta_reg_head = nn.Sequential(
-            nn.Linear(1024, 512),
+            nn.Linear(32, 512),
             nn.ReLU(),
             nn.Linear(512, 256),
             nn.ReLU(),
@@ -24,10 +29,16 @@ class SimpleLineEstimator(nn.Module):
         )
 
     def forward(self, src: torch.Tensor) -> torch.Tensor:
+        # origin shape of src: (batch_size, channel, height, width)
+        # permute the input tensor to (batch, height, width, channel)
+        src = src.permute(0, 2, 3, 1)
         features = self._backbone(src)
 
-        x_logits = self._x_reg_head(features)
-        theta_logits = self._theta_reg_head(features)
+        # reduce the spatial dimension of the feature map from (batch, channel, height, width) to (batch, channel)
+        reduced_features = self._avg_pool(features).squeeze()
+
+        x_logits = self._x_reg_head(reduced_features)
+        theta_logits = self._theta_reg_head(reduced_features)
 
         return torch.cat([x_logits, theta_logits], dim=-1)
 
@@ -45,7 +56,7 @@ class Backbone(nn.Module):
 
 
 def build_model():
-    backbone = Backbone()
+    backbone = templates.build_unetpp(in_channels=3, embed_dims=[32, 64, 128, 256, 512])
     model = SimpleLineEstimator(backbone)
 
     return model
