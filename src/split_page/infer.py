8adfb61a221a7f7bcc76a54fc11abf682f452195
daniel <mio0115@github.com>
2024-10-31 21:00:09 +0800

    modify; display all output of valid_data instead of single image

--- src/split_page/infer.py
+++ src/split_page/infer.py
@@ -18,61 +18,70 @@ if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
-    new_width, new_height = 640, 640
+    new_width, new_height = 1024, 1024
 
-    cv2.namedWindow("splitted image", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("splitted image", width=new_width, height=new_height)
-
-    # path_to_images = os.path.join(os.getcwd(), "data", "example")
-
-    path_to_image = os.path.join(
-        os.getcwd(),
-        "data",
-        "valid_data",
-        "scanned",
-        "images",
+    dir_names = [
+        "B6855",
+        "B6960",
+        "B6995",
+        "C2789",
         "C2797",
-        "no6-1011_092043.tif",
-    )
+        "C2811",
+        "C2926",
+        "C3909",
+        "C3920",
+    ]
     path_to_models = os.path.join(os.getcwd(), "src", "split_page", "checkpoints")
-    image = cv2.imread(os.path.join(path_to_image))
-
-    height, width, *_ = image.shape
-
-    # image = cv2.imread(os.path.join(path_to_images, args.image_name))
-
-    resized_image, _ = resize_with_aspect_ratio(
-        image, target_size=(new_width, new_height)
+    model = torch.load(
+        os.path.join(path_to_models, args.model_name), weights_only=False
     )
 
-    model = build_model()
-    model.load_state_dict(
-        torch.load(os.path.join(path_to_models, args.model_name), weights_only=True)
-    )
     model.to(args.device)
     model.eval()
 
-    with torch.no_grad():
-        in_image = (
-            torch.tensor(resized_image).unsqueeze(0).permute(0, 3, 1, 2).float() / 255.0
+    for dir_name in dir_names:
+        path_to_dir = os.path.join(
+            os.getcwd(), "data", "valid_data", "scanned", "images", dir_name
         )
-        in_image = in_image.to(args.device)
-        logits = model(in_image)
-        line_coords = logits.sigmoid().cpu().numpy()
 
-        line_x_coord = int(line_coords[..., 0].item() * width)
-        line_theta = line_coords[..., 1] * np.pi
+        for file_name in os.listdir(path_to_dir):
+            path_to_image = os.path.join(path_to_dir, file_name)
 
-        point_0 = (line_x_coord, height // 2)
+            cv2.namedWindow("splitted image", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("splitted image", width=new_width, height=new_height)
 
-        line_slope = np.tan(line_theta).item()
+            image = cv2.imread(os.path.join(path_to_image))
 
-        point_1 = (line_x_coord - 1000, int(height // 2 - line_slope * 1000))
-        point_2 = (line_x_coord + 1000, int(height // 2 + line_slope * 1000))
+            height, width, *_ = image.shape
 
-        cv2.line(image, pt1=point_1, pt2=point_2, color=(0, 0, 255), thickness=3)
+            resized_image, _ = resize_with_aspect_ratio(
+                image, target_size=(new_width, new_height)
+            )
 
-        # cv2.imshow("masked image", cv2.cvtColor(masked_image, cv2.COLOR_RGB2BGR))
-        cv2.imshow("splitted image", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            with torch.no_grad():
+                in_image = (
+                    torch.tensor(resized_image).unsqueeze(0).permute(0, 3, 1, 2).float()
+                    / 255.0
+                )
+                in_image = in_image.to(args.device)
+                logits = model(in_image)
+                line_coords = logits.sigmoid().cpu().numpy()
+
+                line_x_coord = int(line_coords[..., 0].item() * width)
+                line_theta = line_coords[..., 1] * np.pi
+
+                point_0 = (line_x_coord, height // 2)
+
+                line_slope = np.tan(line_theta).item()
+
+                point_1 = (line_x_coord - 1000, int(height // 2 - line_slope * 1000))
+                point_2 = (line_x_coord + 1000, int(height // 2 + line_slope * 1000))
+
+                cv2.line(
+                    image, pt1=point_1, pt2=point_2, color=(0, 0, 255), thickness=3
+                )
+
+                # cv2.imshow("masked image", cv2.cvtColor(masked_image, cv2.COLOR_RGB2BGR))
+                cv2.imshow("splitted image", image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
