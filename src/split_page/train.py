0a2114f9c0b6420cdde8072135870e3a0f3aa255
daniel <mio0115@github.com>
2024-11-02 15:16:34 +0800

    modify; milestone in lr_scheduler

--- src/split_page/train.py
+++ src/split_page/train.py
@@ -25,7 +25,7 @@ def train(
     valid: bool = True,
 ):
     print("Training model...")
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 30, 35])
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 40])
     model = model.to(args.device)
     path_to_save = os.path.join(os.getcwd(), "checkpoints", args.save_as)
 
