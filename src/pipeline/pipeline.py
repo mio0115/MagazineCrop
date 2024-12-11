d11e2d800a00a078712ac6324577108d07361f60
daniel <mio0115@github.com>
2024-12-11 16:53:36 +0800

    move get_arguments outside; move valid and set var to init

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -1,4 +1,3 @@
-import time
 import os
 from functools import partial
 
@@ -26,6 +25,14 @@ def load_image(path: str) -> np.ndarray:
     return image
 
 
+def get_arguments(parser_mode: str = "user"):
+    """
+    Get the arguments from the argument parser.
+    """
+    parser = get_parser(parser_mode)
+    return parser.parse_args()
+
+
 class MagazineCropPipeline:
     """
     Orchestrates the magazine cropping pipeline:
@@ -34,82 +41,75 @@ class MagazineCropPipeline:
     3. Initialize required models and classes.
     4. Process the image (split, fix distortion).
     5. Save the resulting pages.
-
-    Instance Variables:
-        _parser_mode (str): Mode for the argument parser.
-        _new_shape (tuple[int, int]): Target shape for image resizing.
-        _split_pages (Combination or None): Object handling page splitting.
-        _fix_distortion (FixDistortion or None): Object to fix page distortion.
     """
 
     def __init__(
         self, parser_mode: str = "user", new_shape: tuple[int, int] = (1024, 1024)
     ):
-        self._parser_mode = parser_mode
-        self._new_shape = new_shape
+        self._tmp_shape = new_shape
 
-        # Instance variables to be set after parsing and validation
+        args = get_arguments(parser_mode)
+        self.validate_arguments(args)
+        self.set_instance_variables(args)
+
+        # Instance variables of pipeline components
         self._split_pages = None
         self._fix_distortion = None
-        self._output_scale = None
-        self._input_path = None
-        self._image_name = None
-        self._output_scale = None
-        self._quality = None
-        self._output_dir = None
-        self._verbose = None
-        self._is_single_page = None
-        self._use_gpu = None
 
-    def valid_and_set(self, args):
+    def validate_arguments(self, args):
         """
         Validate the arguments from argparse to ensure input/output paths are valid.
         """
         if not os.path.isdir(args.output_dir):
             raise FileNotFoundError(f"{args.output_dir} does not exist")
-        self._output_dir = args.output_dir
 
         if not os.path.isfile(args.input):
             raise FileNotFoundError(f"{args.input} does not exist")
-        self._image_name = os.path.basename(args.input).split(".")[0]
-        self._input_path = args.input
 
         if not isinstance(args.output_scale, (int, float)) or (
             isinstance(args.output_scale, (int, float))
             and not (0 < args.output_scale <= 1)
         ):
             raise ValueError("Output scale must be a float between 0 and 1")
-        self._output_scale = args.output_scale
 
         if not isinstance(args.quality, int) or (
             isinstance(args.quality, int) and not (0 < args.quality <= 100)
         ):
             raise ValueError("Quality must be an integer between 0 and 100")
-        self._quality = args.quality
 
+    def set_instance_variables(self, args):
+        """
+        Set the instance variables based on the given arguments.
+        """
+        self._input_path = args.input
+        self._output_dir = args.output_dir
+        self._output_scale = args.output_scale
+        self._quality = args.quality
         self._verbose = args.verbose
-        self._is_single_page = args.single_page
-        self._use_gpu = args.use_gpu
+
+        self._device = "cuda" if args.use_gpu else "cpu"
+        self._num_pages = 1 if args.single_page else 2
+        self._image_name = os.path.basename(self._input_path).split(".")[0]
+        self._save_mask_fn = partial(save_mask, path_to_save=self._output_dir)
 
     def init_pipeline(self):
         """
         Initialize the pipeline components based on the given arguments.
         """
-        device = "cuda" if self._use_gpu else "cpu"
         predict_fg = PredictForeground(
-            device=device, verbose=self._verbose, new_size=self._new_shape
+            device=self._device, verbose=self._verbose, new_size=self._tmp_shape
         )
         predict_sp = SplitPage(
-            device=device, verbose=self._verbose, new_size=self._new_shape
+            device=self._device, verbose=self._verbose, new_size=self._tmp_shape
         )
         self._split_pages = Combination(
             predict_fg,
             predict_sp,
-            num_pages=1 if self._is_single_page else 2,
+            num_pages=self._num_pages,
             verbose=self._verbose,
-            save_mask_fn=partial(save_mask, path_to_save=self._output_dir),
+            save_mask_fn=self._save_mask_fn,
         )
-        self._fix_distortion = FixDistortion(target_size=self._new_shape)
+        self._fix_distortion = FixDistortion(target_size=self._tmp_shape)
 
     def process(self, image: np.ndarray):
         """
@@ -154,30 +154,20 @@ class MagazineCropPipeline:
                 quality=self._quality,
             )
 
-    def get_arguments(self):
-        """
-        Get the arguments from the argument parser.
-        """
-        parser = get_parser(self._parser_mode)
-        return parser.parse_args()
-
     def __call__(self):
         """
         Execute the entire pipeline:
-        1. Parse args
-        2. Validate args
-        3. Initialize pipeline components
-        4. Load input image
-        5. Process image
-        6. Save results
+        1. Initialize pipeline components
+        2. Load input image
+        3. Process image
+        4. Save results
         """
-        args = self.get_arguments()
-        self.valid_and_set(args)
+        # Initialize the pipeline components
         self.init_pipeline()
 
         # Load the input image from the given path
         image = load_image(path=self._input_path)
         # Process the image
         fixed_pages = self.process(image=image)
-        # Scaled if needed and save the result images
+        # Scaled (if needed) and save the result images
         self.save_output(fixed_pages)
