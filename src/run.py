8d3e26e40f4641357a827ddfd5b476be8d52fb19
daniel <mio0115@github.com>
2024-12-12 22:04:41 +0800

    drop redundant code

--- src/run.py
+++ src/run.py
@@ -1,22 +1,13 @@
 import time
 
-from .pipeline.pipeline import MagazineCropPipeline, load_image
+from .pipeline.pipeline import MagazineCropPipeline
 
 
 def main():
     start_time = time.time()
 
     pipeline = MagazineCropPipeline(parser_mode="user")
-
     pipeline()
-
-    # args = pipeline.get_args()
-    # pipeline.valid_and_set(args=args)
-    # pipeline.init_pipeline()
-
-    # image = load_image(path=pipeline._input_path)
-    # fixed_pages = pipeline.process(image=image)
-    # pipeline.save_output(fixed_pages=fixed_pages)
 
     end_time = time.time()
     print(f"Completed in {end_time-start_time:.2f} seconds")
