61fb2c822e63d0f24ee3a63616e511474a96bc6e
daniel <mio0115@github.com>
2025-01-24 14:33:47 +0800

    modify; move to_logits from Backbone to Model

--- src/remove_background/model/mod_unet_pp.py
+++ src/remove_background/model/mod_unet_pp.py
@@ -21,23 +21,17 @@ class Backbone(nn.Module):
         )
 
         self._backbones = nn.ModuleList(backbones)
-
-        self._to_logits = nn.Conv2d(
-            in_channels=backbones[0].out_channels,
-            out_channels=1,
-            kernel_size=1,
-            bias=False,
-        )
+        self.out_channels = backbones[0].out_channels
 
     def forward(self, src):
-        logits = []
+        raws = []
 
         x = self._warmup(src)
 
         x = self._backbones[0](x)
-        logits.append(self._to_logits(x))
+        raws.append(x)
 
-        return logits
+        return raws
 
 
 class Model(nn.Module):
@@ -47,14 +41,23 @@ class Model(nn.Module):
         super(Model, self).__init__(*args, **kwargs)
 
         self._backbone = backbone
+        self._to_logits = nn.Conv2d(
+            in_channels=self._backbone.out_channels,
+            out_channels=1,
+            kernel_size=1,
+            bias=False,
+        )
 
-        self._line_approx_block = LineApproxBlock(src_shape=src_shape)
+        self._line_approx_block = LineApproxBlock(
+            in_channels=self._backbone.out_channels, src_shape=src_shape
+        )
 
     def forward(
         self, src: torch.Tensor, edge_length: torch.Tensor, edge_theta: torch.Tensor
     ) -> dict[str, torch.Tensor]:
-        logits = self._backbone(src)[0]
-        coords = self._line_approx_block(logits, edge_length, edge_theta)
+        raws = self._backbone(src)[0]
+        coords = self._line_approx_block(raws, edge_length, edge_theta)
+        logits = self._to_logits(raws)
         outputs = {"logits": logits, "coords": coords}
 
         return outputs
@@ -93,16 +96,27 @@ def build_backbone(
     )
 
     if resume:
-        model.load_state_dict(torch.load(path_to_ckpt, weights_only=True))
+        state_dict = torch.load(path_to_ckpt, weights_only=True)
+        model.load_state_dict(state_dict)
 
     return model
 
 
 def build_model(
-    path_to_backbone_ckpt: str = None, src_shape: tuple[int, int] = (640, 640)
+    path_to_ckpt: str = None,
+    src_shape: tuple[int, int] = (640, 640),
+    backbone_name: str = "rm_bg_iter_C2980_weights.pth",
+    to_logits_name: str = "rm_bg_to_logits_C2980_weights.pth",
 ):
-    backbone = build_backbone(resume=True, path_to_ckpt=path_to_backbone_ckpt)
+    backbone = build_backbone(
+        resume=True, path_to_ckpt=os.path.join(path_to_ckpt, backbone_name)
+    )
     model = Model(backbone, src_shape=src_shape)
+
+    to_logits_state = torch.load(
+        os.path.join(path_to_ckpt, to_logits_name), weights_only=True
+    )
+    model._to_logits.load_state_dict(to_logits_state)
 
     return model
 
@@ -110,19 +124,15 @@ def build_model(
 if __name__ == "__main__":
     import os
 
-    backbone = build_backbone(
-        resume=True,
+    model = build_model(
         path_to_ckpt=os.path.join(
-            os.getcwd(),
-            "src",
-            "remove_background",
-            "checkpoints",
-            "rm_bg_iter_C2980_part_weights.pth",
-        ),
+            os.getcwd(), "src", "remove_background", "checkpoints"
+        )
     )
-    model = Model(backbone)
+    for key in model.state_dict().keys():
+        print(key)
 
-    img = torch.rand(size=(2, 4, 640, 640))
+    img = torch.rand(size=(1, 4, 640, 640))
     outputs = model(
         img,
         edge_len=torch.tensor([100.0, 105.0], dtype=torch.float32),
