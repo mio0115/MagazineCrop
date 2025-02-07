4b1b1d5ca0bdbcd751518bee49a622f8b24db89d
daniel <mio0115@github.com>
2025-02-07 14:44:06 +0800

    modify; modify lr_finder for seperate testing learning rate for head and backbone

--- src/remove_background/lr_finder.py
+++ src/remove_background/lr_finder.py
@@ -21,10 +21,9 @@ def lr_finder(
     loss_fn,
     optimizer,
     init_lr=1e-7,
-    final_lr=1e-2,
-    num_steps=800,
+    final_lr=1e-1,
+    num_steps=500,
     accumulation_steps: int = 8,
-    batch_size: int = 2,
 ):
     """Implements a simple Learning Rate Finder"""
     model.train()
@@ -33,39 +32,42 @@ def lr_finder(
     scheduler = ExponentialLR(optimizer, gamma=gamma)
 
     step_losses = 0
-    for i, data in enumerate(cycle(train_loader)):
-        if i >= num_steps:
-            break
+    steps, epoch = 0, 1
+    while True:
+        for i, data in enumerate(train_loader):
+            if steps >= num_steps:
+                return lrs, losses
 
-        inputs, targets, weights = [
-            move_to_device(data=x, device=args.device) for x in data
-        ]
+            inputs, targets, weights = [
+                move_to_device(data=x, device=args.device) for x in data
+            ]
 
-        # outputs = {'logits': logits, 'coords': coords}
-        outputs = model(
-            src=inputs["images"],
-            edge_length=inputs["length"],
-            edge_theta=inputs["theta"],
-        )
+            # outputs = {'logits': logits, 'coords': coords}
+            outputs = model(
+                src=inputs["images"],
+                edge_length=inputs["length"],
+                edge_theta=inputs["theta"],
+            )
 
-        loss, *_ = loss_fn(outputs=outputs, targets=targets, weights=weights)
-        loss = loss / accumulation_steps
-        loss.backward()
+            loss, *_ = loss_fn(outputs=outputs, targets=targets, weights=weights)
+            loss = loss / accumulation_steps
+            loss.backward()
 
-        step_losses += loss.item()
+            step_losses += loss.item()
 
-        if (i + 1) % accumulation_steps == 0:
-            optimizer.step()
-            optimizer.zero_grad()
-            scheduler.step()
+            if (i + 1) % accumulation_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+                scheduler.step()
 
-            losses.append(step_losses)
-            step_losses = 0
+                losses.append(step_losses)
+                step_losses = 0
 
-            backbone_lrs.append(optimizer.param_groups[0]["lr"])
-            lrs.append(optimizer.param_groups[-1]["lr"])
+                lrs.append(optimizer.param_groups[0]["lr"])
 
-    return lrs, backbone_lrs, losses
+                steps += 1
+        print(f"Epoch {epoch} Step {steps}")
+        epoch += 1
 
 
 if __name__ == "__main__":
@@ -97,36 +99,10 @@ if __name__ == "__main__":
     )
     model = model.to(args.device)
 
-    loss_fn = ModComboLoss(number_of_classes=1, factor=0.3)
-    iou_metric = IOUMetric(
-        height=src_shape[0], width=src_shape[1], reduction="mean"
-    ).to(args.device)
-
-    optimizer = torch.optim.AdamW(
-        [
-            {"params": model._backbone.parameters(), "lr": args.lr_backbone},
-            {"params": model._to_logits.parameters(), "lr": args.lr_backbone},
-            {
-                "params": [
-                    param
-                    for name, param in model.named_parameters()
-                    if "_backbone" not in name and "_to_logits" not in name
-                ]
-            },
-        ],
-        lr=args.learning_rate,
-    )
-
     train_dataset = ModMagazineCropDataset(
         split="train",
         transforms=build_scanned_transform(split="train", size=src_shape),
         augment_factor=args.augment_factor,
-        edge_size=args.edge_size,
-    )
-    valid_dataset = ModMagazineCropDataset(
-        split="valid",
-        transforms=build_scanned_transform(split="valid", size=src_shape),
-        augment_factor=2,
         edge_size=args.edge_size,
     )
     dataloader = {
@@ -137,34 +113,78 @@ if __name__ == "__main__":
             num_workers=args.dataloader_workers,
             collate_fn=mod_mc_collate_fn,
         ),
-        "valid": DataLoader(
-            valid_dataset,
-            batch_size=args.batch_size,
-            shuffle=False,
-            num_workers=args.dataloader_workers,
-            collate_fn=mod_mc_collate_fn,
-        ),
     }
+    loss_fn = ModComboLoss(number_of_classes=1, factor=0.3)
+    iou_metric = IOUMetric(
+        height=src_shape[0], width=src_shape[1], reduction="mean"
+    ).to(args.device)
 
+    # For rest part of model
+    for param in model._backbone.parameters():
+        param.requires_grad = False
+    for param in model._to_logits.parameters():
+        param.requires_grad = False
+    for name, param in model.named_parameters():
+        if "_backbone" not in name and "_to_logits" not in name:
+            param.requires_grad = True
+    optimizer = torch.optim.AdamW(
+        [
+            {
+                "params": [
+                    param
+                    for name, param in model.named_parameters()
+                    if "_backbone" not in name and "_to_logits" not in name
+                ]
+            },
+        ],
+        lr=args.learning_rate,
+    )
     # Run LR Finder
-    lrs, backbone_lrs, losses = lr_finder(
+    lrs, losses = lr_finder(
         model,
         dataloader["train"],
         loss_fn,
         optimizer,
         accumulation_steps=args.accumulation_steps,
-        batch_size=args.batch_size,
     )
-    print("LEARNING RATE")
-    for lr, loss in zip(lrs, losses):
-        print(f"learning rate: {lr:.6f}, loss: {loss:.6f}")
-    print("BACKBONE LEARNING RATE")
-    for lr, loss in zip(backbone_lrs, losses):
-        print(f"learning rate: {lr:.6f}, loss: {loss:.6f}")
     plt.plot(lrs, losses)
-    # plt.plot(backbone_lrs, losses)
     plt.xscale("log")
     plt.xlabel("Learning Rate")
+    plt.ylabel("Loss")
+    plt.title("Learning Rate Finder")
+    plt.show()
+
+    # For backbone
+    model = build_model(
+        path_to_ckpt=args.checkpoint_dir,
+        src_shape=src_shape,
+    )
+    model = model.to(args.device)
+
+    for param in model._backbone.parameters():
+        param.requires_grad = True
+    for param in model._to_logits.parameters():
+        param.requires_grad = True
+    for name, param in model.named_parameters():
+        if "_backbone" not in name and "_to_logits" not in name:
+            param.requires_grad = False
+    optimizer = torch.optim.AdamW(
+        [
+            {"params": model._backbone.parameters()},
+            {"params": model._to_logits.parameters()},
+        ],
+        lr=args.learning_rate,
+    )
+    lrs, losses = lr_finder(
+        model,
+        dataloader["train"],
+        loss_fn,
+        optimizer,
+        accumulation_steps=args.accumulation_steps,
+    )
+    plt.plot(lrs, losses)
+    plt.xscale("log")
+    plt.xlabel("Backbone Learning Rate")
     plt.ylabel("Loss")
     plt.title("Learning Rate Finder")
     plt.show()
