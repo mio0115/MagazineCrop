6090cf7aefa96d193f6b96b00de5909046927939
daniel <mio0115@github.com>
2025-02-19 11:56:34 +0800

    complete lr_finder

--- src/remove_background/lr_finder.py
+++ src/remove_background/lr_finder.py
@@ -1,18 +1,9 @@
 from torch.optim.lr_scheduler import ExponentialLR
 import torch
 import matplotlib.pyplot as plt
-from itertools import cycle
+from math import log10
 
-
-def move_to_device(
-    data: dict[str, torch.Tensor] | torch.Tensor, device: str | torch.device
-) -> dict[str, torch.Tensor]:
-    if isinstance(data, torch.Tensor):
-        return data.to(device)
-    elif isinstance(data, dict):
-        return {k: v.to(device) for k, v in data.items()}
-    else:
-        raise ValueError("Data should be either a dict or a tensor")
+from ..utils.misc import move_to_device
 
 
 def lr_finder(
@@ -27,37 +18,52 @@ def lr_finder(
 ):
     """Implements a simple Learning Rate Finder"""
     model.train()
-    lrs, losses, backbone_lrs = [], [], []
+    lrs, losses = [], []
     gamma = (final_lr / init_lr) ** (1 / num_steps)
     scheduler = ExponentialLR(optimizer, gamma=gamma)
 
     step_losses = 0
     steps, epoch = 0, 1
-    while True:
-        for i, data in enumerate(train_loader):
-            if steps >= num_steps:
-                return lrs, losses
 
+    scaler = torch.GradScaler(args.device)
+    while True:
+        for ind, data in enumerate(train_loader, start=1):
             inputs, targets, weights = [
                 move_to_device(data=x, device=args.device) for x in data
             ]
 
-            # outputs = {'logits': logits, 'coords': coords}
-            outputs = model(
-                src=inputs["images"],
-                edge_length=inputs["length"],
-                edge_theta=inputs["theta"],
-            )
+            # outputs = {'logits': logits,}
+            with torch.autocast(device_type=args.device, dtype=torch.float16):
+                outputs = model(
+                    src=inputs["images"],
+                )
 
-            loss, *_ = loss_fn(outputs=outputs, targets=targets, weights=weights)
+                loss = loss_fn(
+                    logits=outputs["logits"],
+                    targets=targets["labels"],
+                    weights=weights,
+                )
+
             loss = loss / accumulation_steps
-            loss.backward()
+            scaler.scale(loss).backward()
+            if torch.isnan(loss):
+                step_losses += 1.0 / accumulation_steps
+            else:
+                step_losses += loss.item()
 
-            step_losses += loss.item()
+            if ind % accumulation_steps == 0:
+                scaler.unscale_(optimizer)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
 
-            if (i + 1) % accumulation_steps == 0:
-                optimizer.step()
+                for name, param in model.named_parameters():
+                    if param.grad is not None and torch.isnan(param.grad).any():
+                        print(f"Warning: NaN detected in gradients of {name}.")
+                        break
+
+                scaler.step(optimizer)
+                scaler.update()
                 optimizer.zero_grad()
+
                 scheduler.step()
 
                 losses.append(step_losses)
@@ -66,6 +72,8 @@ def lr_finder(
                 lrs.append(optimizer.param_groups[0]["lr"])
 
                 steps += 1
+                if steps == num_steps:
+                    return lrs, losses
         print(f"Epoch {epoch} Step {steps}")
         epoch += 1
 
@@ -76,34 +84,23 @@ if __name__ == "__main__":
     import torch
     from torch.utils.data import DataLoader
 
-    from .model.mod_unet_pp import build_model
-    from .datasets import ModMagazineCropDataset, mod_mc_collate_fn
-    from .mod_transforms import (
+    from .model.model_unet3p import build_model
+    from .datasets import MagazineCropDataset, mc_collate_fn
+    from .transforms import (
         build_scanned_transform,
     )
     from ..utils.arg_parser import get_parser
-    from .loss import ModComboLoss
-    from .metrics import IOUMetric
+    from .loss import ComboLoss
 
     parser = get_parser("dev")
     args = parser.parse_args()
 
-    path_to_train = os.path.join(os.getcwd(), "data", "train_data")
-    path_to_valid = os.path.join(os.getcwd(), "data", "valid_data")
-
     src_shape = (args.edge_size, args.edge_size)
-    # model = build_iterative_model(num_iter=3, num_class=1)
-    model = build_model(
-        path_to_ckpt=args.checkpoint_dir,
-        src_shape=src_shape,
-    )
-    model = model.to(args.device)
 
-    train_dataset = ModMagazineCropDataset(
+    train_dataset = MagazineCropDataset(
         split="train",
-        transforms=build_scanned_transform(split="train", size=src_shape),
+        transforms=build_scanned_transform(split="train", reshape_size=args.edge_size),
         augment_factor=args.augment_factor,
-        edge_size=args.edge_size,
     )
     dataloader = {
         "train": DataLoader(
@@ -111,80 +108,96 @@ if __name__ == "__main__":
             batch_size=args.batch_size,
             shuffle=True,
             num_workers=args.dataloader_workers,
-            collate_fn=mod_mc_collate_fn,
+            collate_fn=mc_collate_fn,
         ),
     }
-    loss_fn = ModComboLoss(number_of_classes=1, factor=0.3)
-    iou_metric = IOUMetric(
-        height=src_shape[0], width=src_shape[1], reduction="mean"
-    ).to(args.device)
+    loss_fn = ComboLoss(number_of_classes=1)
+
+    model = build_model(in_channels=3, out_channels=1)
+    model = model.to(args.device)
+
+    param_count = sum(p.numel() for p in model.parameters())
+    if param_count < 10**6:  # Small model
+        init_lr = 1e-6
+        final_lr = 1e-2
+    elif param_count < 10**8:  # Medium model
+        init_lr = 1e-7
+        final_lr = 1e-1
+    else:  # Large model
+        init_lr = 1e-8
+        final_lr = 1e-1
+    num_steps = int(log10(final_lr / init_lr)) * 50
 
     # For rest part of model
-    for param in model._backbone.parameters():
+    for param in model.parameters():
+        param.requires_grad = True
+    for param in model.encoders.parameters():
         param.requires_grad = False
-    for param in model._to_logits.parameters():
-        param.requires_grad = False
-    for name, param in model.named_parameters():
-        if "_backbone" not in name and "_to_logits" not in name:
-            param.requires_grad = True
     optimizer = torch.optim.AdamW(
         [
             {
                 "params": [
                     param
                     for name, param in model.named_parameters()
-                    if "_backbone" not in name and "_to_logits" not in name
+                    if "encoders" not in name
                 ]
             },
         ],
-        lr=args.learning_rate,
+        lr=init_lr,
+        eps=1e-5,
     )
     # Run LR Finder
     lrs, losses = lr_finder(
-        model,
-        dataloader["train"],
-        loss_fn,
-        optimizer,
+        model=model,
+        train_loader=dataloader["train"],
+        loss_fn=loss_fn,
+        init_lr=init_lr,
+        final_lr=final_lr,
+        num_steps=num_steps,
+        optimizer=optimizer,
         accumulation_steps=args.accumulation_steps,
     )
     plt.plot(lrs, losses)
     plt.xscale("log")
     plt.xlabel("Learning Rate")
     plt.ylabel("Loss")
-    plt.title("Learning Rate Finder")
-    plt.show()
+    plt.title("Learning Rate Finder - Head")
+
+    save_path = os.path.join("/", "home", "daniel", "Pictures", "lr_finder_head.pdf")
+    plt.savefig(save_path, dpi=600, format="pdf")
+    plt.clf()
 
     # For backbone
-    model = build_model(
-        path_to_ckpt=args.checkpoint_dir,
-        src_shape=src_shape,
-    )
+    model = build_model(in_channels=3, out_channels=1)
     model = model.to(args.device)
 
-    for param in model._backbone.parameters():
+    for param in model.parameters():
+        param.requires_grad = False
+    for param in model.encoders.parameters():
         param.requires_grad = True
-    for param in model._to_logits.parameters():
-        param.requires_grad = True
-    for name, param in model.named_parameters():
-        if "_backbone" not in name and "_to_logits" not in name:
-            param.requires_grad = False
     optimizer = torch.optim.AdamW(
         [
-            {"params": model._backbone.parameters()},
-            {"params": model._to_logits.parameters()},
+            {"params": model.encoders.parameters()},
         ],
-        lr=args.learning_rate,
+        lr=init_lr,
+        eps=1e-5,
     )
     lrs, losses = lr_finder(
-        model,
-        dataloader["train"],
-        loss_fn,
-        optimizer,
+        model=model,
+        train_loader=dataloader["train"],
+        loss_fn=loss_fn,
+        init_lr=init_lr,
+        final_lr=final_lr,
+        num_steps=num_steps,
+        optimizer=optimizer,
         accumulation_steps=args.accumulation_steps,
     )
     plt.plot(lrs, losses)
     plt.xscale("log")
     plt.xlabel("Backbone Learning Rate")
     plt.ylabel("Loss")
-    plt.title("Learning Rate Finder")
-    plt.show()
+    plt.title("Learning Rate Finder - Backbone")
+    save_path = os.path.join(
+        "/", "home", "daniel", "Pictures", "lr_finder_backbone.pdf"
+    )
+    plt.savefig(save_path, dpi=600, format="pdf")
